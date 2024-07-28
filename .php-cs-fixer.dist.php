a868f73f47107067f72591461b3ba448e3aadf8b
raideer <hi@kristofers.dev>
2024-07-28 15:46:53 +0300

    adds php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'no_unused_imports' => true
+    ])
+    ->setFinder($finder)
+;
